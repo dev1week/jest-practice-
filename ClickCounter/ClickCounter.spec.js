9c389d9b3827b5143225d31e4e362b53b07c7f3e
dev1week <rondo2860@gmail.com>
2026-05-05 17:57:55 +0900

    test: add a test for spec2 : increase

--- ClickCounter/ClickCounter.spec.js
+++ ClickCounter/ClickCounter.spec.js
@@ -8,4 +8,12 @@ describe("ClickCounter", () => {
       expect(counter.getValue()).toBe(0);
     });
   });
+
+  describe("increase()", () => {
+    it("카운터를 1 올린다.", () => {
+      const counter = ClickCounter();
+      counter.increase();
+      expect(counter.getValue()).toBe(1);
+    });
+  });
 });
