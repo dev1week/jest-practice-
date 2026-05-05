e6d04a5abd6be107eb37c991988c68c277e4e081
dev1week <rondo2860@gmail.com>
2026-05-05 21:56:57 +0900

    feat: ClickCountView 버튼 클릭 이벤트 리스너 구현

--- ClickCounter/ClickCounterView.js
+++ ClickCounter/ClickCounterView.js
@@ -1,22 +1,26 @@
-export function ClickCounterView(clickCounter, updateEl) {
+export function ClickCounterView(clickCounter, optionEl) {
   if (!clickCounter) {
     throw Error();
   }
 
-  if (!updateEl) {
+  if (!optionEl.updateEl) {
     throw Error();
   }
 
   // 메서드를 꺼내 호출해도 this 바인딩이 깨지지 않게, 동일 인스턴스를 api로 두고 메서드 간에 참조한다.
   const api = {
     updateView() {
-      updateEl.innerHTML = String(clickCounter.getValue());
+      optionEl.updateEl.innerHTML = String(clickCounter.getValue());
     },
     increaseAndUpdateView() {
       clickCounter.increase();
       api.updateView();
     },
   };
+
+  optionEl.triggerEl.addEventListener("click", () => {
+    api.increaseAndUpdateView();
+  });
 
   return api;
 }
