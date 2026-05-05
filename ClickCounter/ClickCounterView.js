53dbb46ca5ee3f86808910849c5b818d8bf64229
dev1week <rondo2860@gmail.com>
2026-05-05 20:20:00 +0900

    feat: ClickCounterView 모듈에서 인자 검증 로직 추가

--- ClickCounter/ClickCounterView.js
+++ ClickCounter/ClickCounterView.js
@@ -1,4 +1,12 @@
 export function ClickCounterView(clickCounter, updateEl) {
+  if (!clickCounter) {
+    throw Error();
+  }
+
+  if (!updateEl) {
+    throw Error();
+  }
+
   return {
     updateView() {
       updateEl.innerHTML = String(clickCounter.getValue());
